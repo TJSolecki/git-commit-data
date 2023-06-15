9a3c34a624d29f5433e94db465dd6afc6548c79c
Thomas Solecki <thomasjsolecki@gmail.com>
2023-06-15 11:25:52 -0700

    working with single page

--- index.js
+++ index.js
@@ -1,32 +1,71 @@
 const { Octokit } = require("octokit");
-const githubApiKey = process.env.GITHUB_API_KEY;
+const githubApiKey = process.env.API_KEY_2;
+
+const authorAndCommitsMade = {};
 
 const octokit = new Octokit({
   auth: githubApiKey
 });
 
-const getTitleAndAuthor = async () => {
-  const result = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+const getBranchesOfRepo = async () => {
+  const result = await octokit.request("GET /repos/{owner}/{repo}/branches", {
     owner: "FasterXML",
     repo: "jackson-databind",
   }).then( (responce) => {
-    const authorAndCommitsMade = {};
+    const branches = [];
+    const branchesMetaData = responce.data;
+    for (const branchData of branchesMetaData) {
+      // Get the branch
+      const branch = branchData.name;
+      branches.push(branch)
+    }
+    console.log(branches);
+  for(const b of branches) {
+    console.log(b)
+    getTitleAndAuthor(b);
+  }
+  }).catch( (error) => {
+    return `There was an error: ${error}`;
+  })
+
+}
+
+const getTitleAndAuthor = async (branch) => {
+  const result = await octokit.request("GET /repos/{owner}/{repo}/commits/", {
+    owner: "FasterXML",
+    repo: "jackson-databind",
+    branch: branch,
+  }).then( (responce) => {
+    console.log(`hey from ${branch}`)
     const commits = responce.data;
+    console.log(commits);
     for (const commit of commits) {
       // Get the author
-      const name = commit.committer.login;
+      console.log(commit);
+      const name = commit.author.login;
       if(authorAndCommitsMade[name] != null){
         authorAndCommitsMade[name]++;
       }
       else {
-        console.log('a')
         authorAndCommitsMade[name] = 1;
       }
     }
     console.log(authorAndCommitsMade);
+    return authorAndCommitsMade
   }).catch( (error) => {
     return `There was an error: ${error}`;
   })
 };
 
-console.log(getTitleAndAuthor());
+// getTitleAndAuthor()
+async function main() {
+  const branches = await getBranchesOfRepo();
+  console.log(branches);
+  // for(const branch in branches) {
+  //   getTitleAndAuthor(branch);
+  // }
+  // console.log(authorAndCommitsMade);
+  // getTitleAndAuthor("master");
+}
+main();
+
